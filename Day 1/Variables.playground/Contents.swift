174b4627341b889b45bd261dc9a9f3938cad1632
Luke Lazzaro <makorenwastaken@gmail.com>
2021-09-17 10:55:34 +1000

    Initial commit

---
+++ Day 1/Variables.playground/Contents.swift
@@ -0,0 +1,25 @@
+var str = "Hello playground!"
+str = "Other string"
+
+var age = 38
+var population = 8_000_000
+
+var multiline = """
+This goes over
+multiple lines.
+"""
+
+var multiline2 = """
+This goes over \
+multiple lines \
+without the line break character.
+"""
+
+var pi = 3.141
+var bool = true
+
+var interp = "Your age is \(age)"
+
+let const = "This is a constant string."
+
+let typeAnnotation: String = "This is explicit typing."
