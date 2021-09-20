ee3946ef12f839bbe7897275ff2ad12a5909d181
Luke Lazzaro <makorenwastaken@gmail.com>
2021-09-20 10:12:49 +1000

    Day 4

---
+++ Day 4/Loops.playground/Contents.swift
@@ -0,0 +1,69 @@
+let count = 1...10
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+print("Players gonna ")
+for _ in 1...5 {
+    print("play")
+}
+
+var number = 1
+while number <= 20 {
+    print(number)
+    number += 1
+}
+print("Ready or not, here I come!")
+
+var number2 = 1
+repeat {
+    print(number2)
+    number2 += 1
+} while number <= 20
+print("Ready or not, here I come!")
+
+var countDown = 10
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    
+    countDown -= 1
+}
+print("Blast off!")
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+for i in 1...10 {
+    if i % 2 == 1 {
+        continue
+    }
+    print(i)
+}
+
+var counter = 0
+while true {
+    print(" ")
+    counter += 1
+    if counter == 273 {
+        break
+    }
+}
