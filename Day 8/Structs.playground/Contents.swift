92dd00cf11f2b9c5f9b799080bd8f326e69cb474
Luke Lazzaro <makorenwastaken@gmail.com>
2021-09-24 12:00:20 +1000

    Day 8

---
+++ Day 8/Structs.playground/Contents.swift
@@ -0,0 +1,61 @@
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+var tennis = Sport(name: "Tennis", isOlympicSport: true)
+print(tennis.name)
+tennis.name = "Lawn tennis"
+print(tennis.olympicStatus)
+
+struct Race {
+    var runners = 0
+    var distance: Int
+}
+let race = Race(distance: 5)
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+var toys = ["Woody"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
+
+let tens = [30, 20, 10]
+tens.sorted()
