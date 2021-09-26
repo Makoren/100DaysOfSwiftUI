4c6cbcb38ef9e4a2364ec749564bc14fb5726927
Luke Lazzaro <makorenwastaken@gmail.com>
2021-09-27 09:38:38 +1000

    Day 11

---
+++ Day 11/Protocols.playground/Contents.swift
@@ -0,0 +1,71 @@
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+displayID(thing: User(id: "Luke"))
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation {}
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+    
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+let number = 8
+number.squared()
+number.isEven
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+pythons.summarize()
+beatles.summarize()
+
+protocol Identifiable2 {
+    var id: String { get set }
+    func identify()
+}
+
+extension Identifiable2 {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User2: Identifiable2 {
+    var id: String
+}
+let makoren = User2(id: "makoren")
+makoren.identify()
