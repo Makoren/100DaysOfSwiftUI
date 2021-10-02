f95149c7e35c9b976afff2cde8b6840992eadda5
Luke Lazzaro <makorenwastaken@gmail.com>
2021-10-03 10:07:25 +1100

    Day 17

---
+++ Day 17/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Luke Lazzaro on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
