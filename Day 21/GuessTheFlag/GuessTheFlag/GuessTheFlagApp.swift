93c012f292843e14392b70c7aea28fb58cb42511
Luke Lazzaro <makorenwastaken@gmail.com>
2021-10-07 09:46:31 +1100

    Day 21

---
+++ Day 21/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Luke Lazzaro on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
