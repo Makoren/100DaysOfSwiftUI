d7ad01649f77432e304afdb08c0cd879026a56c8
Luke Lazzaro <makorenwastaken@gmail.com>
2021-10-14 09:33:19 +1100

    Day 28

---
+++ Day 28/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Luke Lazzaro on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
