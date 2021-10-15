3281f1961664ba9989d732408dbe1e1d6b67730d
Luke Lazzaro <makorenwastaken@gmail.com>
2021-10-16 09:55:35 +1100

    Day 30

---
+++ Day 30/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Luke Lazzaro on 15/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
