ac1e3cd324169acc8c4dea9996ba1f4fbb2f0b3b
Luke Lazzaro <makorenwastaken@gmail.com>
2021-11-10 09:36:26 +1100

    Day 55

---
+++ Day 55/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Luke Lazzaro on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
