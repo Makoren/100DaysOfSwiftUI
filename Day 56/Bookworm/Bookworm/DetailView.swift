bf8c7a70379eea734a527fab99d15d2c7bc26168
Luke Lazzaro <makorenwastaken@gmail.com>
2021-11-11 09:36:38 +1100

    Day 56

---
+++ Day 56/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,98 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Luke Lazzaro on 10/11/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+
+    let book: Book
+    
+    var dateFormatted: String {
+        if let date = book.dateAdded {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MMM d, YYYY"
+            return formatter.string(from: date)
+        } else {
+            return "No date provided"
+        }
+    }
+
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geo.size.width)
+                    
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+                
+                Text(self.book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text("Added: \(self.dateFormatted)")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Text(self.book.review ?? "No review")
+                    .padding()
+                
+                RatingView(rating: .constant(Int(self.book.rating)))
+                    .font(.largeTitle)
+            }
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            Button(action: {
+                self.showingDeleteAlert = true
+            }) {
+                Image(systemName: "trash")
+            }
+        }
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                self.deleteBook()
+            }, secondaryButton: .cancel())
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        try? self.moc.save()
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+    
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
