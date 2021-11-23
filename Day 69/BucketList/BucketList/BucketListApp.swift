bd781f55db4980ef7c37ad7d29d222edbd1d33bb
Luke Lazzaro <makorenwastaken@gmail.com>
2021-11-24 09:24:06 +1100

    Day 69

---
+++ Day 69/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Luke Lazzaro on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
