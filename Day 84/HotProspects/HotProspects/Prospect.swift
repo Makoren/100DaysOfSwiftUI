4c813b15c75c36c49b80793721b11312f0f77eda
Luke Lazzaro <makorenwastaken@gmail.com>
2021-12-09 10:07:58 +1100

    Day 84

---
+++ Day 84/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,49 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Luke Lazzaro on 7/12/21.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    static let saveKey = "SavedData"
+    @Published private(set) var people: [Prospect]
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+    
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        // always call send() before changing properties to make sure there are no animation bugs
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+}
