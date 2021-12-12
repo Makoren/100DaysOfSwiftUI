2825c8cbd4d04254ccb2b384fde5e3c1e00770ad
Luke Lazzaro <zodiac44@hotmail.com>
2021-12-13 09:33:06 +1100

    Day 88

---
+++ Day 88/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Luke Lazzaro on 11/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
+    }
+}
